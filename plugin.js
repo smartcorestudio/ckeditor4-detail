1e85d970b7575a888490ba4159a86cef6ecc87ba
Ayhan Akilli <info@ayhan.biz>
2018-02-13 14:41:19 +0100

    Another approach

--- plugin.js
+++ plugin.js
@@ -9,20 +9,48 @@
         init: function (editor) {
             editor.widgets.add('detail', {
                 button: editor.lang.detail.title,
-                template: '<details><summary>Summary</summary><p>Content</p></details>',
+                template: '<details><summary>Summary</summary><p data-details></p><div data-details><p>Content</p></div></details>',
                 editables: {
-                    summary: {
+                    p: {
                         selector: 'p[data-details]'
                     },
-                    content: {
+                    div: {
                         selector: 'div[data-details]',
                         allowedContent: 'br p em s strong sub sup u; a[!href,target]'
                     }
                 },
                 allowedContent: 'details summary',
-                requiredContent: 'details; summary; div[data-details]',
+                requiredContent: 'details; summary; p[data-details]; div[data-details]',
                 upcast: function (el) {
-                    return el.name === 'details' && el.children && el.children[0].name === 'summary';
+                    if (el.name !== 'details' || el.children.length <= 0 || el.children[0].name !== 'summary') {
+                        return false;
+                    }
+
+                    if (el.children.length !== 3
+                        || el.children[1].name !== 'p' || !el.children[1].attributes.hasOwnProperty('data-details')
+                        || el.children[2].name !== 'div' || !el.children[2].attributes.hasOwnProperty('data-details')
+                    ) {
+                        var p, div;
+
+                        if (el.children.length >= 2 && el.children[1].name === 'p' && el.children[1].attributes.hasOwnProperty('data-details')) {
+                            p = el.children[1];
+                            el.add(p, 1);
+                        } else {
+                            p = new CKEDITOR.htmlParser.element('p', {'data-details': ''});
+                        }
+
+                        if (el.children.length >= 3 && el.children[2].name === 'div' && el.children[2].attributes.hasOwnProperty('data-details')) {
+                            div = el.children[2];
+                        } else {
+                            div = new CKEDITOR.htmlParser.element('div', {'data-details': ''});
+                            div.children = el.children.slice(2);
+                            el.add(div);
+                        }
+
+                        el.children = el.children.slice(0, 3);
+                    }
+
+                    return true;
                 },
                 downcast: function (el) {
                     var div = el.children[2];
@@ -30,35 +58,13 @@
                     el.children = el.children.slice(0, 1).concat(div.children);
                 },
                 init: function () {
-                    var el = this.element;
-                    var summary, p, div;
+                    var summary = this.element.getChild(0);
+                    var p = this.element.getChild(1);
 
-                    if (el.getChildren().length !== 3
-                        || el.getChild(1).getName() !== 'p'
-                        || !el.getChild(1).getAttribute('data-details')
-                        || el.getChild(2).getName() !== 'div'
-                        || !el.getChild(2).getAttribute('data-details')
-                    ) {
-
-                        div = new CKEDITOR.dom.element('div');
-                        div.setAttribute('data-details', true);
-                        el.moveChildren(div);
-                        div.getChild(0).move(el);
-                        p = new CKEDITOR.dom.element('p');
-                        p.setAttribute('data-details', true);
-                        p.appendTo(el);
-                        div.appendTo(el);
-                    }
-
-                    summary = el.getChild(0);
-                    p = el.getChild(1);
-                    p.setAttribute('contenteditable', true);
                     p.setHtml(summary.getHtml());
                     p.on('blur', function () {
                         summary.setHtml(p.getHtml());
                     });
-                    div.getChild(2);
-                    div.setAttribute('contenteditable', true);
                 }
             });
         }
