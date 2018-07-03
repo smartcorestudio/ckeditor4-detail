f1171b30fcdde3799ad4236b724c246ce437438f
Ayhan Akilli <info@ayhan.biz>
2018-07-03 11:38:28 +0200

    Refactor plugin

--- plugin.js
+++ plugin.js
@@ -9,29 +9,44 @@
         init: function (editor) {
             editor.widgets.add('detail', {
                 button: editor.lang.detail.title,
-                template: '<details><summary>Summary</summary><p data-details></p><div data-details><p>Content</p></div></details>',
+                template: '<details><summary>Summary</summary><p class="details-summary"></p><div class="details-content"><p>Content</p></div></details>',
                 editables: {
-                    p: {
-                        selector: 'p[data-details]'
+                    summary: {
+                        selector: 'p.details-summary'
                     },
-                    div: {
-                        selector: 'div[data-details]'
+                    content: {
+                        selector: 'div.details-content'
                     }
                 },
                 allowedContent: 'details summary',
-                requiredContent: 'details; summary; p[data-details]; div[data-details]',
+                requiredContent: 'details; summary; p(details-summary); div(details-content)',
                 upcast: function (el) {
-                    if (el.name !== 'details' || el.children.length <= 0 || el.children[0].name !== 'summary') {
+                    if (el.name !== 'details') {
                         return false;
                     }
 
-                    if (el.children.length < 2 || el.children[1].name !== 'p' || !el.children[1].attributes.hasOwnProperty('data-details')) {
-                        el.add(new CKEDITOR.htmlParser.element('p', {'data-details': ''}), 1);
+                    if (el.children.length <= 0 || el.children[0].name !== 'summary') {
+                        var summary = new CKEDITOR.htmlParser.element('summary');
+
+                        summary.add(new CKEDITOR.htmlParser.text('Summary'));
+                        el.add(summary, 0);
                     }
 
-                    if (el.children.length !== 3 || el.children[2].name !== 'div' || !el.children[2].attributes.hasOwnProperty('data-details')) {
-                        var div = new CKEDITOR.htmlParser.element('div', {'data-details': ''});
-                        div.children = el.children.slice(2);
+                    if (el.children.length < 2 || el.children[1].name !== 'p' || !el.children[1].hasClass('details-summary')) {
+                        el.add(new CKEDITOR.htmlParser.element('p', {'class': 'details-summary'}), 1);
+                    }
+
+                    if (el.children.length !== 3 || el.children[2].name !== 'div' || !el.children[2].hasClass('details-content')) {
+                        var div = new CKEDITOR.htmlParser.element('div', {'class': 'details-content'});
+
+                        if (el.children.length > 2) {
+                            div.children = el.children.slice(2);
+                        } else {
+                            var p = new CKEDITOR.htmlParser.element('p');
+                            p.add(new CKEDITOR.htmlParser.text('Content'));
+                            div.add(p);
+                        }
+
                         el.add(div, 2);
                         el.children = el.children.slice(0, 3);
                     }
@@ -41,7 +56,7 @@
                 downcast: function (el) {
                     el.attributes = [];
                     el.children = el.children.slice(0, 1);
-                    el.setHtml(el.getHtml() + this.editables['div'].getData());
+                    el.setHtml(el.getHtml() + this.editables.content.getData());
                 },
                 init: function () {
                     var summary = this.element.getChild(0);
