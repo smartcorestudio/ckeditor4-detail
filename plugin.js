d88d0ec24269b7438117d8d73e62769b22613f9d
Ayhan Akilli <info@ayhan.biz>
2018-07-27 16:27:19 +0200

    Refactor widget -> inline editing version

--- plugin.js
+++ plugin.js
@@ -1,78 +1,89 @@
 'use strict';
 
 (function (CKEDITOR) {
-    var h = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
-
     CKEDITOR.plugins.add('detail', {
         requires: 'widget',
         icons: 'detail',
         hidpi: true,
         lang: 'de,en',
         init: function (editor) {
+            var formatTags = editor.config.format_tags.split(';');
+            var h = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].filter(function (item) {
+                return formatTags.indexOf(item) >= 0;
+            });
+
             editor.widgets.add('detail', {
                 button: editor.lang.detail.title,
-                template: '<details><summary>Summary</summary><p class="head"></p><div class="content"><p>Content</p></div></details>',
+                template: '<div class="details"><p class="details-summary">Summary</p><div class="details-content"></div></div>',
                 editables: {
-                    head: {
-                        selector: '.head'
+                    summary: {
+                        selector: '.details-summary',
+                        allowedContent: {}
                     },
                     content: {
-                        selector: '.content'
+                        selector: '.details-content'
                     }
                 },
                 allowedContent: 'details summary',
-                requiredContent: 'details; summary; ' + h.join(' ') + ' p(head); div(content)',
+                requiredContent: 'details; summary',
                 upcast: function (el) {
                     if (el.name !== 'details') {
                         return false;
                     }
 
-                    var summary;
-                    var head = 'p';
-                    var content = new CKEDITOR.htmlParser.element('div', {'class': 'content'});
+                    var editSummary = new CKEDITOR.htmlParser.element('p', {'class': 'details-summary'});
+                    var editContent = new CKEDITOR.htmlParser.element('div', {'class': 'details-content'});
+                    var first = el.getFirst('summary');
+                    var html = 'Summary';
 
-                    if (el.children.length > 0 && el.children[0].name === 'summary') {
-                        summary = el.children[0];
-                    } else {
-                        summary = new CKEDITOR.htmlParser.element('summary');
-                        el.add(summary, 0);
+                    el.name = 'div';
+                    el.addClass('details');
+                    el.add(editSummary, 0);
+                    el.add(editContent, 1);
+
+                    if (first) {
+                        if (first.children.length > 0 && first.children[0].type === CKEDITOR.NODE_ELEMENT) {
+                            if (h.indexOf(first.children[0].name) >= 0) {
+                                editSummary.attributes['data-details-head'] = first.children[0].name;
+                            }
+
+                            html = first.children[0].getHtml();
+                        } else if (first.children.length > 0 && first.children[0].type === CKEDITOR.NODE_TEXT) {
+                            html = first.children[0].value;
+                        }
+
+                        first.remove();
                     }
 
-                    if (summary.children.length <= 0) {
-                        summary.add(new CKEDITOR.htmlParser.text('Summary'));
-                    } else if (summary.children[0].type === CKEDITOR.NODE_ELEMENT && h.indexOf(summary.children[0].name) >= 0) {
-                        head = summary.children[0].name;
-                    }
-
-                    el.add(new CKEDITOR.htmlParser.element(head, {'class': 'head'}), 1);
+                    editSummary.setHtml(html);
 
                     if (el.children.length > 2) {
-                        content.children = el.children.slice(2);
-                    } else {
-                        var p = new CKEDITOR.htmlParser.element('p');
-                        p.add(new CKEDITOR.htmlParser.text('Content'));
-                        content.add(p);
+                        editContent.children = el.children.slice(2);
+                        el.children = el.children.slice(0, 2);
                     }
-
-                    el.add(content, 2);
-                    el.children = el.children.slice(0, 3);
 
                     return true;
                 },
                 downcast: function (el) {
-                    el.attributes = [];
-                    el.children = el.children.slice(0, 1);
-                    el.setHtml(el.getHtml() + this.editables.content.getData());
-                },
-                init: function () {
-                    var summary = this.element.getChild(0);
-                    var head = this.element.getChild(1);
-                    var ref = h.indexOf(head.getName()) >= 0 ? summary.getChild(0) : summary;
+                    var editSummary = el.children[0];
+                    var editContent = el.children[1];
+                    var summary = new CKEDITOR.htmlParser.element('summary');
+                    var headEl = editSummary.attributes['data-details-head'] || null;
 
-                    head.setHtml(ref.getHtml());
-                    head.on('blur', function () {
-                        ref.setHtml(head.getHtml());
-                    });
+                    el.add(summary, 0);
+
+                    if (!!editSummary.attributes['data-details-head']) {
+                        var head = new CKEDITOR.htmlParser.element(headEl);
+                        head.setHtml(editSummary.getHtml());
+                        summary.add(head);
+                    } else {
+                        summary.setHtml(editSummary.getHtml());
+                    }
+
+                    editSummary.remove();
+                    editContent.replaceWithChildren();
+                    el.name = 'details';
+                    el.attributes = [];
                 }
             });
         }
